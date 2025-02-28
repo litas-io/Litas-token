1f6a136e221e679c70d7f5c212cb6b16a0374594
BeAWhale <info@beawhale.io>
2025-02-28 12:02:27 +0200

    "Ownable" import removed

--- token.sol
+++ token.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Litas is ERC20, ReentrancyGuard {
